5dc0d7930e55957450fa2406af7999328179205e
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2020-08-26 22:04:59 +0700

    Error Handlers

---
+++ server/app.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+const blogRouter = require("./routes/blogs");
+
+const mongoose = require("mongoose");
+const databaseUrl = "mongodb://localhost:27017/website";
+mongoose.connect(databaseUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.once("open", (_) => {
+  console.log("Database Connected to:", databaseUrl);
+});
+db.on("error", (err) => {
+  console.error("Connection Error:", err);
+});
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+//TODO: Add authentication
+app.use("/blogs", blogRouter);
+
+app.use((err, req, res, next) => {
+  //Client Side Error
+  console.log(err.name);
+  if (
+    ["VallidationError", "CastError", "DocumentNotFoundError"].includes(
+      err.name
+    )
+  ) {
+    //Make each error a unique message
+    res.status(400).json({ name: err.name, message: err.message });
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res) => {
+  //Server Side Error
+  console.log({ stack: err.stack, name: err.name });
+  res.status(500);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening in PORT :${PORT}`));
