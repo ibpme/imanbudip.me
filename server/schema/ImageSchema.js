c0b8a391cf12644af148af79abd624d4ef45b086
Iman-Budi Pranakaish <ibpranakasih@gmail.com>
2020-09-09 00:54:57 +0700

    remove author and add camera

--- server/schema/ImageSchema.js
+++ server/schema/ImageSchema.js
@@ -2,21 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const imageSchema = new Schema({
-  image: Buffer,
+  image: { type: Buffer, required: true },
   caption: String,
-  author: String,
   dateCreated: { type: Date, default: Date.now() },
   dateModified: { type: Date, default: Date.now() },
   url: String,
+  camera: String,
+  fileSize: { type: Number, required: true },
   album: {
     title: String,
-    id: String,
+    id: { type: String, required: true },
     url: String,
   },
   hidden: { type: Boolean, default: false },
-  meta: {
-    tags: String,
-  },
 });
 
 module.exports = mongoose.model("Images", imageSchema);
